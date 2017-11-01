dd1cc08adc35de5572157fb7fc95695907626ac7
Ryan Fitzpatrick <email@theryanfitzpatrick.com>
2017-11-01 17:18:28 -0400

    Pushing code structure and first files

---
+++ Headers/Num.h
@@ -0,0 +1,14 @@
+#ifndef _CH_NUM_
+#define _CH_NUM_
+
+#ifndef _STDINT_
+#define _STDINT_
+#include <stdint.h>
+#endif
+
+typedef int32_t ch_num;
+
+ch_num CH_NumPlus2(ch_num, ch_num);
+void CH_NumPlus(ch_num *, ch_num);
+
+#endif
