cb85a9bb4219df2833edff4b840128074927e877
Ryan Fitzpatrick <email@theryanfitzpatrick.com>
2017-11-07 23:09:25 -0500

    Update Memory.h
    Added ResizeMem macro

--- Headers/Memory.h
+++ Headers/Memory.h
@@ -12,8 +12,19 @@
 /* Allocates a new pointer using malloc */
 /* NOTE: If malloc fails this macro will jump to the FAIL label, so the calling code must have a FAIL label */
 /* Param1 ptr: The pointer to be allocated */
-/* Param2 size: The size of the memory to point at, clean up wit DiscardMem */
+/* Param2 size: The size of the memory to point at, clean up with DiscardMem */
 #define NewMem(ptr, size) if ((ptr = malloc(size)) == NULL) goto FAIL
+
+/* Resizes a pointer using realloc */
+/* NOTE: If realloc fails this macro will jump to the FAIL label, so the calling code must have a FAIL label */
+/* Param1 temp: A temp pointer used to hold the return value of realloc and check for any error */
+/* Param2 ptr: The pointer to be reallocated */
+/* Param2 size: The size of the memory to point at, clean up with DiscardMem */
+#define ResizeMem(temp, ptr, size) \
+    if ((temp = realloc(ptr, size)) == NULL) \
+        goto FAIL; \
+\
+    ptr = temp
 
 /* Releases a pointer using free */
 /* Param1 ptr: The pointer to be released */
